606541305f681ee9f889cb41490d41e9f98fb58f
Tasnim Khandakar <tasnimk@berkeley.edu>
2016-11-15 23:11:30 -0800

    users model finished

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  
+  get '/create', to: 'user#create' #change to events#create
+  patch 'create' => 'user#create' #change to events#create
 end
