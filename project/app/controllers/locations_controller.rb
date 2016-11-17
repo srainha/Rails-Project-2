00a9f1a99227d8552eeaddbfc20a1e10dd3b7b26
Lauren Benitez <laurenbenitez@berkeley.edu>
2016-11-16 16:46:01 -0800

    added locations

---
+++ project/app/controllers/locations_controller.rb
@@ -0,0 +1,8 @@
+class LocationsController < ApplicationController
+	def new
+		@location = Location.new
+	end
+
+	def create
+	end
+end
--- project/db/schema.rb
+++ project/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116181527) do
+ActiveRecord::Schema.define(version: 20161117003141) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "room"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
