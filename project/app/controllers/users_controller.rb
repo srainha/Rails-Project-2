25bca82a970bef9a5631f71aed751be67ed83232
Lauren Benitez <laurenbenitez@berkeley.edu>
2016-11-26 19:11:09 -0800

    idk

--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def test
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     unless @user == current_user
