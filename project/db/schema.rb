c9218b0595f8228a21c6cf773b38a0cf38a219d7
Seth Rainha <sethrainha@Seths-MacBook-Pro-3.local>
2016-11-28 20:10:41 -0800

    added images and fixed text

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -29,13 +29,6 @@ ActiveRecord::Schema.define(version: 20161128234312) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "locations", force: :cascade do |t|
-    t.string   "name"
-    t.string   "room"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "resources", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -5,5 +5,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+
+
+
+soda = Location.create(name: "Soda", room: "10")
+wheeler = Location.create(name: "Wheeler", room: "45")
+moffit = Location.create(name: "Moffit", room: "Basement")
+blueDoor = Location.create(name: "Blue Door Cafe", room: "Main room")
+
+ruby = Course.create(name: "Ruby on Rails Decal")
+english = Course.create(name: "English R1B")
+greek = Course.create(name: "Greek History")
+art = Course.create(name: "Art 101")
+
+seth = User.create(name: "Seth", email: "seth@seth.com")
+lauren = User.create(name: "Lauren", email: "lauren@lauren.com")
+zhi = User.create(name: "Zhi", email: "zhi@zhi.com")
+lai = User.create(name: "Lai", email: "lai@lai.com")
+
+Event.new(course: ruby, 
+    					 user: seth, 
+    					 time: "Monday 10am", 
+    					 location: soda,
+    					 upcoming: true)
+
+Event.new(course: english, 
+    					 user: lauren, 
+    					 time: "Friday noon", 
+    					 location: wheeler,
+    					 upcoming: true)
+
+Event.new(course: greek, 
+    					 user: zhi, 
+    					 time: "Saturday from 5-10pm", 
+    					 location: moffit,
+    					 upcoming: true)
+Event.new(course: art, 
+    					 user: lai, 
+    					 time: "Sunday evening", 
+    					 location: blueDoor,
+    					 upcoming: true)
+
+
+
+
